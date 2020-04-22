6eb8379f6918b6566c4be51146b7500550a6aa31
paslavskyi9roman <paslavsky.roman@gmail.com>
2020-04-22 20:57:42 +0300

    🚗

---
+++ script.js
@@ -0,0 +1,39 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+// Functions
+
+function addTodo(event) {
+  // Prevent form form submitting
+  event.preventDefault();
+
+  // ToDo div
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+
+  // Create Li
+  const newTodo = document.createElement('li');
+  newTodo.innerText = 'hey';
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+
+  // Checked mark button
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add('complete-btn');
+  todoDiv.appendChild(completedButton);
+
+  // Checked trash button
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add('complete-btn');
+  todoDiv.appendChild(trashButton);
+
+  // Append to list
+  todoList.appendChild(todoDiv);
+}
